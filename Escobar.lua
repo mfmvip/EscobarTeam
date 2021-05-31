93907c98dc2ee0631ff755652084889493ce14d7
mfmvip <80322856+mfmvip@users.noreply.github.com>
2021-06-01 00:43:55 +0300

    Update Escobar.lua

--- Escobar.lua
+++ Escobar.lua
@@ -3682,7 +3682,7 @@ Dev_mfm(msg.chat_id_, msg.id_, 1, text, 1, 'html')
 end
 getUser(msg.sender_user_id_,get_firstname)
 end   
-if text == "مبرمج السورس" then  Dev_mfm(msg.chat_id_, msg.id_, 1, '✘ ∫ @F16_ibra', 1, 'md') end
+if text == "مبرمج السورس" then  Dev_mfm(msg.chat_id_, msg.id_, 1, '✘ ∫ [@F16_ibra]', 1, 'md') end
 if text == "مطور السورس" then  Dev_mfm(msg.chat_id_, msg.id_, 1, '✘ ∫ @MFMVIP', 1, 'md') end
 if text == "اهمس" or text == "همسه" or text == "اريد بوت الهمسه" or text == "دزلي بوت الهمسه" or  text == "دزولي بوت الهمسه" then  Dev_mfm(msg.chat_id_, msg.id_, 1, '✘ ∫ @NNBBOT', 1, 'md') end
 if text == "رابط حذف" or text == "رابط الحذف" or text == "اريد رابط الحذف" or  text == "شمرلي رابط الحذف" or text == "اريد رابط حذف" then local inline = {{{text="اضغط هنا",url="https://my.telegram.org/auth"}}} SendInline(msg.chat_id_,'✘ ∫ اضغط للحصول على الرابط',nil,inline) return false end
